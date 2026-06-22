5ee0bb6c22021c91c2ee8439410a20150d3121c5
yinnonlerner-alt <yinnonlerner@gmail.com>
2026-06-22 13:01:34 +0300

    1

--- user.java
+++ user.java
@@ -1,9 +1,14 @@
-public class user {
+/**
+ * מחלקה המייצגת משתמש או פעולה במערכת הנדל"ן.
+ */
+public class User {
     public static void main(String[] args){
-        int x = 5;
-        int y = 10;
-        int sum = x + y;
-        System.out.println(sum);
-
+        // שימוש בשמות ברורים למניעת בלבול בין חברי הצוות
+        int basePropertyPrice = 5;
+        int additionalTaxes = 10;
+        
+        int finalPriceCalculated = basePropertyPrice + additionalTaxes;
+        
+        System.out.println("The final price is: " + finalPriceCalculated);
     }
 }
