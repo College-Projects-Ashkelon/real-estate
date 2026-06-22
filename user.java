0672d2df6fcbcd9437dac150cedb8bd45860808d
yinnonlerner-alt <yinnonlerner@gmail.com>
2026-06-22 13:17:39 +0300

    1

--- user.java
+++ user.java
@@ -2,13 +2,30 @@
  * מחלקה המייצגת משתמש או פעולה במערכת הנדל"ן.
  */
 public class User {
+    public static boolean isStrongPassword(String password){
+        boolean hasDigit = false;
+        boolean hasSpecialChar = false;
+        for (int i = 0; i < password.length; i++) {
+            char c = password.charAt(i);
+
+            // בדיקה האם התו הוא ספרה
+            if (Character.isDigit(c)) {
+                hasDigit = true;
+            }
+
+            // בדיקה האם התו הוא תו מיוחד (לא אות, לא ספרה ולא רווח)
+            if (!Character.isLetterOrDigit(c) && !Character.isWhitespace(c)) {
+                hasSpecialChar = true;
+            }
+        }
+        if (hasDigit == true && hasSpecialChar == true){
+            return true;
+        }
+        return false;
+
+        return hasDigit && hasSpecialChar;
+    }
     public static void main(String[] args){
-        // שימוש בשמות ברורים למניעת בלבול בין חברי הצוות
-        int basePropertyPrice = 5;
-        int additionalTaxes = 10;
-        
-        int finalPriceCalculated = basePropertyPrice + additionalTaxes;
-        
-        System.out.println("The final price is: " + finalPriceCalculated);
+        System.out.println(isStrongPassword(123y!i));
     }
 }
